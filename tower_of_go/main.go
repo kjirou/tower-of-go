3602a1ff7c8a4f6d76056bc2ac6f8ab8c1f314a9
kjirou <sorenariblog@gmail.com>
2020-04-02 22:41:04 +0900

    Create and render a matrix

---
+++ tower_of_go/main.go
@@ -0,0 +1,66 @@
+package main
+
+// TODO:
+// - gofmt
+
+import (
+	"fmt"
+	"strings"
+)
+
+type FieldElement struct {
+	Y int
+	X int
+	Symbol string
+}
+
+type FieldMatrix [][]FieldElement
+
+func createFieldMatrix(y int, x int) FieldMatrix {
+	matrix := make(FieldMatrix, y)
+	for rowIndex := 0; rowIndex < y; rowIndex++ {
+		row := make([]FieldElement, x)
+		for columnIndex := 0; columnIndex < x; columnIndex++ {
+			row[columnIndex] = FieldElement{
+				rowIndex,
+				columnIndex,
+				".",
+			}
+		}
+		matrix[rowIndex] = row
+	}
+	return matrix
+}
+
+func measureFieldMatrixY(fieldMatrix FieldMatrix) int {
+	return len(fieldMatrix)
+}
+
+func measureFieldMatrixX(fieldMatrix FieldMatrix) int {
+	return len(fieldMatrix[0])
+}
+
+func renderFieldElement(fieldElement FieldElement) string {
+	return fieldElement.Symbol
+}
+
+func renderFieldMatrix(fieldMatrix FieldMatrix) string {
+	y := measureFieldMatrixY(fieldMatrix)
+	x := measureFieldMatrixX(fieldMatrix)
+	lines := make([]string, y)
+	for rowIndex := 0; rowIndex < y; rowIndex++ {
+		line := ""
+		// Use mapping method
+		for columnIndex := 0; columnIndex < x; columnIndex++ {
+			line += renderFieldElement(fieldMatrix[rowIndex][columnIndex])
+		}
+		lines[rowIndex] = line
+	}
+	return strings.Join(lines, "\n")
+}
+
+func main() {
+	fieldMatrix := createFieldMatrix(12, 20)
+	output := renderFieldMatrix(fieldMatrix)
+	fmt.Println(output)
+}
