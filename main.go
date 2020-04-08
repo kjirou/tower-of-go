bce72d19965adc60738ff4034dc2e855f3767920
kjirou <sorenariblog@gmail.com>
2020-04-08 21:49:48 +0900

    Add a todo

--- main.go
+++ main.go
@@ -35,6 +35,13 @@ func (controller *Controller) Dispatch(newState *models.State) {
 func drawTerminal(screen *views.Screen) {
 	for y, row := range screen.GetMatrix() {
 		for x, element := range row {
+			// TODO: おそらく termbox.SetCell() の終了は非同期で、
+			//         同 cell へ高速で重複して出力した場合に互いの出力バッファが入れ子になってしまう。
+			//       たまに、端末の画面に ANSI の破片らしき文字列がが出力されることがあることが根拠。
+			//       現在は、メインループによる再描画とキー操作による再描画が重なることがあり、
+			//         それで高速に重複して同じ cell へ出力することがある。
+			//       解決案は、キー操作による状態更新もメインループで解決するようにすることで、
+			//         これでおそらくはほとんど発生しなくなると思う。
 			termbox.SetCell(x, y, element.Symbol, element.ForegroundColor, element.BackgroundColor)
 		}
 	}
