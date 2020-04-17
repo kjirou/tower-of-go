c05cf9ab2c9d45ae8851f4d098620c5bf38d7a84
kjirou <sorenariblog@gmail.com>
2020-04-17 23:42:19 +0900

    Add test cases

--- utils/utils_test.go
+++ utils/utils_test.go
@@ -12,7 +12,11 @@ func TestMatrixPosition_GetX(t *testing.T) {
 		fields fields
 		want   int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "returns X value",
+			fields: fields{X: 2, Y: 1},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -37,7 +41,11 @@ func TestMatrixPosition_GetY(t *testing.T) {
 		fields fields
 		want   int
 	}{
-		// TODO: Add test cases.
+		{
+			name: "returns Y value",
+			fields: fields{X: 1, Y: 2},
+			want: 2,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -67,7 +75,36 @@ func TestMatrixPosition_Validate(t *testing.T) {
 		args   args
 		want   bool
 	}{
-		// TODO: Add test cases.
+		{
+			name: "returns false when Y is less than 0",
+			fields: fields{X: 0, Y: -1},
+			args: args{rowLength: 1, columnLength: 1},
+			want: false,
+		},
+		{
+			name: "returns false when X is less than 0",
+			fields: fields{X: -1, Y: 0},
+			args: args{rowLength: 1, columnLength: 1},
+			want: false,
+		},
+		{
+			name: "returns false when Y is greater than or equal to rowLength",
+			fields: fields{X: 0, Y: 1},
+			args: args{rowLength: 1, columnLength: 1},
+			want: false,
+		},
+		{
+			name: "returns false when X is greater than or equal to columnLength",
+			fields: fields{X: 1, Y: 0},
+			args: args{rowLength: 1, columnLength: 1},
+			want: false,
+		},
+		{
+			name: "returns true if Y is greater than 0 and less than rowLength and X is greater than 0 and less than columnLength",
+			fields: fields{X: 0, Y: 0},
+			args: args{rowLength: 1, columnLength: 1},
+			want: true,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
